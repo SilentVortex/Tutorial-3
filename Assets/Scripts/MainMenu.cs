0df8d1525089240ef00dd97c44f4bac02bf25ece
SilentVortex <149170463+SilentVortex@users.noreply.github.com>
2025-01-13 11:16:01 +0000

    finished!!!

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -1,22 +1,20 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
-    // Load scene //
     public void playLevel1()
     {
         SceneManager.LoadScene("Level1");
+        Debug.Log("Player loaded Lev");
     }
 
     public void playMainMenu()
     {
         SceneManager.LoadScene("MainMenu");
+        Debug.Log("Player loaded main menu");
     }
 
-    // Quit game //
     public void quit()
     {
         Application.Quit();
